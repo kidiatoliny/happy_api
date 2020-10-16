8a3e2ae83fbbf90ddce58a72d2178c0c38076ed8
kid <kidiatoliny@gmail.com>
2020-10-16 01:43:03 -0100

    data validation

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -2,7 +2,7 @@ import {Request, Response} from 'express'
 import { getRepository } from 'typeorm'
 import Orphanages from '../models/Orphanages'
 import orphanageView from '../views/orphanages.view'
-
+import * as Yup from 'yup'
 export default {
 
   /** get all orphanages */
@@ -50,7 +50,7 @@ export default {
     }
   })
 
-  const orphanages = orphanagesRepository.create({
+  const data = {
     name,
     latitude,
     longitude,
@@ -59,7 +59,28 @@ export default {
     opening_hours,
     open_on_weekends,
     images
+  }
+
+  const schema  = Yup.object().shape({
+
+    name:Yup.string().required(),
+    latitude:Yup.number().required(),
+    longitude:Yup.number().required(),
+    about:Yup.string().required().max(300),
+    instructions:Yup.string().required(),
+    opening_hours:Yup.string().required(),
+    open_on_weekends:Yup.string().required(),
+    images:Yup.array(
+      Yup.object().shape({
+        path:Yup.string().required()
+    }))
   })
+
+
+  await schema.validate(data, {
+    abortEarly:false
+  })
+  const orphanages = orphanagesRepository.create(data)
 
   await orphanagesRepository.save(orphanages)
 
